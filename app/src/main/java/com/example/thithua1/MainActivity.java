c028e25588e6f6ae91b1e2b684bb0dc8b3d62011
Trần Lê Quân <tranlequan09082001@gmail.com>
2022-12-21 22:01:29 +0700

    second

--- app/src/main/java/com/example/thithua1/MainActivity.java
+++ app/src/main/java/com/example/thithua1/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.thithua1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView rvPerson;
+    ArrayList<person_class> listPerson;
+    PersonAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvPerson = findViewById(R.id.rvPerson);
+        listPerson = person_class.init();
+        rvPerson.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new PersonAdapter(this,listPerson);
+        rvPerson.setAdapter(adapter);
     }
 }
